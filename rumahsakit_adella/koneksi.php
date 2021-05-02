d83d6ee4b768ad181ce43c8a849f14077ddb9892
aadellaptr <59139739+aadellaptr@users.noreply.github.com>
2021-05-02 13:55:39 +0700

    Delete rumahsakit_adella directory

--- rumahsakit_adella/koneksi.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (empty($_SESSION["email"])){
-    $_SESSION["alert"] = "Anda Harus Login Terlebih Dahulu";
-    header ("Location:login.php");
-}
-?>
--- rumahsakit_adella/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION["email"]);
-$_SESSION["alert"] = "Anda berhasil Logout";
-header("Location:login.php");
-?>
